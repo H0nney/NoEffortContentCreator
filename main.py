3191f11c3c6485a3dd8fa9d3a006cc301823ee71
Hny <jakublipinski99@gmail.com>
2023-03-19 14:47:47 +0100

    TTS

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
+import pyttsx3
+
 from urllib.request import urlopen
 import json
 
-# json brany z url https://www.reddit.com/r/Showerthoughts/top/.json?t=all&limit=2
+class textToSpeech:
+    engine: pyttsx3.Engine
+    
+    def __init__(self, voice, rate: int, volume: float):
+        self.engine = pyttsx3.init()
+        if voice:
+            self.engine.setProperty('voice', voice)
+            
+        self.engine.setProperty('rate', rate)
+        self.engine.setProperty('volume', volume)
+        
+    def list_available_voices(self):
+        voices: list = self.engine.getProperty('voices')
+        
+        for i, voice in enumerate(voices):
+            print(f'{i}: {voice.name} {voice.age} - {voice.languages} - {voice.gender}, {voice.id}')
+            
+    def say(self, text: str, save: bool = False, file_name='output.mp3'):
+        self.engine.say(text)
+        self.engine.runAndWait()
+        
+        if save:
+            self.engine.save_to_file(text, file_name)
+            
+        self.engine.runAndWait()
+            
 
-# Open json from file posts.json
-postsJson = json.load(open('posts.json'))
-# foreach post in postsJson
-for post in postsJson['data']['children']:
-    print(post['data']['title'])
-    # post content
-    print(post['data']['selftext'])
+if __name__ == '__main__':
+    tts = textToSpeech("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0", 165, 0.8)
+    tts.list_available_voices()
+    
+    # postsJson = json.load(open('posts.json'))
+    # for post in postsJson['data']['children']:
+    #     postTitle = post['data']['title']
+    #     postContent = post['data']['selftext']
+    #     tts.say(postTitle)
+    
+    
+# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0
+    
+# json brany z url https://www.reddit.com/r/AmITheAssHole/top/.json?t=all&limit=2
+    
+    
